413fcb83e4d8022792f02f360ed4e19e21cca79f
Jimmy Chuang <ogre0403@gmail.com>
2025-03-18 16:29:06 +0800

    build: standardize Docker build process with versioned tools
    - Add a new Dockerfile with specific versions for protobuf tools
    - Remove old Dockerfiles: Dockerfile-4.22, Dockerfile-5.27, Dockerfile-5.28
    - Update build.sh script to use build arguments for Docker builds
    
    Signed-off-by: Jimmy Chuang <ogre0403@gmail.com>

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+ARG PROTOC_VER=27.2
+ARG PROTOC_GEN_GO_VER=1.34.2
+ARG PROTOC_GEN_GO_GRPC_VER=1.5.1
+ARG PROTOC_GO_INJECT_TAG_VER=1.4.0
+
+FROM ociscloud/golang:1.22.4-ubuntu-amd64
+
+
+WORKDIR /app
+
+ARG PROTOC_GEN_GO_VER
+ARG PROTOC_GEN_GO_GRPC_VER
+ARG PROTOC_GO_INJECT_TAG_VER
+RUN go install google.golang.org/protobuf/cmd/protoc-gen-go@v${PROTOC_GEN_GO_VER} && \
+    go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v${PROTOC_GEN_GO_GRPC_VER} && \
+	go install github.com/favadi/protoc-go-inject-tag@v${PROTOC_GO_INJECT_TAG_VER}
+
+
+ARG PROTOC_VER
+RUN wget https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VER}/protoc-${PROTOC_VER}-linux-x86_64.zip && \
+    unzip protoc-${PROTOC_VER}-linux-x86_64.zip
+
+
+ENV PATH="/app/bin:${PATH}"
+
+
--- build.sh
+++ build.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-docker build -t ociscloud/protobuf:5.27.2- -f Dockerfile-5.27 .
-docker build -t ociscloud/protobuf:5.28.0- -f Dockerfile-5.28 .
-docker build -t ociscloud/protobuf:4.22.0- -f Dockerfile-4.22 .
+docker build \
+	--build-arg PROTOC_VER=27.2 \
+	--build-arg PROTOC_GEN_GO_VER=1.34.2 \
+	--build-arg PROTOC_GEN_GO_GRPC_VER=1.5.1 \
+	--build-arg PROTOC_GO_INJECT_TAG_VER=1.4.0 \
+	-t ociscloud/protobuf:5.27.2- .
+
+docker build \
+	--build-arg PROTOC_VER=28.0 \
+	--build-arg PROTOC_GEN_GO_VER=1.34.2 \
+	--build-arg PROTOC_GEN_GO_GRPC_VER=1.5.1 \
+	--build-arg PROTOC_GO_INJECT_TAG_VER=1.4.0 \
+	-t ociscloud/protobuf:5.28.0- .
+
+docker build \
+	--build-arg PROTOC_VER=22.0 \
+	--build-arg PROTOC_GEN_GO_VER=1.28.1 \
+	--build-arg PROTOC_GEN_GO_GRPC_VER=1.3.0 \
+	--build-arg PROTOC_GO_INJECT_TAG_VER=1.4.0 \
+	-t ociscloud/protobuf:4.22.0- .
